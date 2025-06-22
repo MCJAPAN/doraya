8d870f0770ac8610101192ebe52540c21a70151a
MCJP_ <aibou_tokumei2013@hotmail.com>
2025-06-22 21:56:05 +0900

    Update script.js

--- data/js/script.js
+++ data/js/script.js
@@ -1,3 +1,12 @@
+   if ('serviceWorker' in navigator) {
+     navigator.serviceWorker.register('/service-worker.js')
+       .then(function(registration) {
+         console.log('Service Worker registered with scope:', registration.scope);
+       })
+       .catch(function(error) {
+         console.log('Service Worker registration failed:', error);
+       });
+   }
 function updateClock() {
     const now = new Date();
     const time = now.toLocaleTimeString("ja-JP", {
